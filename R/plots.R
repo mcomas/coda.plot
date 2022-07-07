a8f87d5f26b6ea4d53213c16031c3618f901bb0e
mcomas <marc.comas@gmail.com>
2022-07-07 11:15:49 +0200

    0.1.4

--- R/plots.R
+++ R/plots.R
@@ -43,7 +43,7 @@ geometric_mean_bar_plot = function(X, group, group_label = "", type = 'bar',
 
 geometric_mean_line_plot = function(X, group, group_label = "", se = FALSE,
                                     title = 'Geometric mean plot',
-                                       xlab = '', ylab = ''){
+                                    xlab = '', ylab = ''){
   dplot = geometric_mean_dplot(X, group)
 
   W = 0.3
@@ -67,41 +67,64 @@ geometric_mean_line_plot = function(X, group, group_label = "", se = FALSE,
 }
 
 #' @export
-biplot_form = function(X, group = NULL, group_label = "", ipc = 1:2){
-  ivar = c(1,1+ipc)
-  PC = coordinates(X, 'pc')
-  DATA = setnames(as.data.table(scale(PC, scale = FALSE))[,..ipc], c('x','y'))
+clr_biplot = function(X, group = NULL, alpha = 1){
+  CLR = coordinates(X, 'clr')
+  SVD = svd(scale(CLR, scale = FALSE))
+
+  DATA = with(SVD, u %*% diag(d^alpha))
+  VARS = with(SVD, diag(d^(1-alpha)) %*% v)
+
+  D2 = as.data.table(DATA)
+  V2 = as.data.table(VARS)
+
+  # D2_min = min(D2)
+  # D2_max = max(D2)
+  # V2_min = min(V2)
+  # V2_max = max(V2)
+
+  V2[,rn := sprintf('clr(%s)', names(X))]
+
+  ipc = 1:2
+  ipc_rn = c(ipc, ncol(V2))
+  V2.long = melt(V2[,..ipc_rn], id.vars = 'rn')
+  V2.length = V2.long[,.(length = sqrt(sum(value^2))),.(rn)]
+  V2.length_max = max(V2.length[,-1])
+
+  D2.long = melt(D2[,..ipc][,id:=1:nrow(D2)], id.vars = 'id')
+  D2.length = D2.long[,.(length = sqrt(sum(value^2))),.(id)]
+  D2.length_max = max(D2.length[,-1])
+
+  max.radius = D2.length_max
+
+  V2.arrow = dcast(V2.long[,.(variable=variable, value=max.radius*value/V2.length_max),.(rn)],
+                   rn~variable, value.var = 'value')
+  V2.radius = dcast(V2.long[,.(variable=variable, value=max.radius*value/sqrt(sum(value^2))),.(rn)],
+                    rn~variable, value.var = 'value')
+
+  # min_ = min(D2,V2)
+  # max_ = max(D2,V2)
   if(!is.null(group)){
     G = data.table(group = group)
   }
-  max.radius = max(abs(DATA))
-  VARS.length_all = (ncol(X) - 1) / ncol(X)
-  VARS = setnames(as.data.table(basis(PC), keep.rownames = TRUE)[,..ivar], c('rn', 'x', 'y'))
-  VARS.long = melt(VARS, id.vars = 'rn')
-  VARS.length_max = max(VARS.long[,.(length = sqrt(sum(value^2))),.(rn)][,-1])
 
-  VARS.arrow = dcast(VARS.long[,.(variable=variable, value=max.radius*VARS.length_max*value/VARS.length_all),.(rn)],
-                      rn~variable, value.var = 'value')
-  VARS.radius = dcast(VARS.long[,.(variable=variable, value=max.radius*value/sqrt(sum(value^2))),.(rn)],
-                      rn~variable, value.var = 'value')
+  pl_ = ggplot()
 
-  pl_ = ggplot() +
-    # geom_hline(yintercept = 0, linetype = 'dashed', alpha = 0.3) +
-    # geom_vline(xintercept = 0, linetype = 'dashed', alpha = 0.3) +
-    geom_segment(data=VARS.radius, aes(x = 0, y = 0, xend = x, yend = y), linetype = 'dotted', size = 0.5,
-                 col = '#db0bb2') + #arrow = arrow(length = unit(0.03, "npc")),
-    geom_segment(data=VARS.arrow, aes(x = 0, y = 0, xend = x, yend = y), size = 1,
-                 col = '#db0bb2')
   if(is.null(group)){
-    pl_ = pl_ + geom_point(data=DATA, aes(x = x, y = y),  col = 'blue', alpha = 0.5)
+    pl_ = pl_ + geom_point(data=D2, aes(x = V1, y = V2))
   }else{
-    pl_ = pl_ + geom_point(data=cbind(G,DATA), aes(x = x, y = y, col = group),  alpha = 0.5)
+    pl_ = pl_ + geom_point(data=cbind(G,D2), aes(x = V1, y = V2, col = group))
   }
   pl_  +
-    # geom_point(aes(x = 0, y = 0), size = 6, shape = 18, color = 'blue') +
-    geom_text(data=VARS.radius, aes(x = x, y = y, label = rn), fontface = 'bold', check_overlap = TRUE) +
-    theme_minimal() + coord_equal(xlim = c(-max.radius, max.radius), ylim = c(-max.radius, max.radius)) +
-    labs(x = sprintf("PC %d", ipc[1]), y = sprintf("PC %d", ipc[2]), col = group_label)
-
+    geom_segment(data=V2.radius, aes(x = 0, y = 0, xend = V1, yend = V2), color = 'blue', linetype = 'dotted', size = 0.5) + #arrow = arrow(length = unit(0.03, "npc")),
+    geom_segment(data=V2.arrow, aes(x = 0, y = 0, xend = V1, yend = V2), color = 'blue', size = 1) +
+    geom_text(data=V2.radius, aes(x = V1, y = V2, label = rn), fontface = 'bold') +
+    lims(x = max.radius*c(-1,1), y = max.radius*c(-1,1))
 }
 
+clr_biplot_cov = function(X, group = NULL){
+  clr_biplot(X, group, alpha = 0)
+}
+
+clr_biplot_form = function(X, group = NULL){
+  clr_biplot(X, group, alpha = 1)
+}
