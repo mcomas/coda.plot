dc3e5ba1324d60701b152236e35015b3cf6840b7
mcomas <marc.comas@gmail.com>
2023-12-01 06:58:15 +0100

    more options

--- R/geometric_mean_barplot.R
+++ R/geometric_mean_barplot.R
@@ -19,37 +19,60 @@ geometric_mean_dplot = function(X, group){
 
 }
 #' @export
-geometric_mean_barplot = function(X, group, group_label = "", type = 'bar',
-                                   title = 'Geometric mean bar plot', xlab = '', ylab = 'ln(group mean)-ln(overall mean)'){
+geometric_mean_barplot = function(X, group, group_label = "",
+                                  title = 'Geometric mean bar plot',
+                                  xlab = '',
+                                  ylab = 'ln(group mean)-ln(overall mean)',
+                                  facets = TRUE, x_show_parts = TRUE){
   dplot = geometric_mean_dplot(X, group)
-  p = ggplot() +
-    geom_hline(yintercept = 0) +
-    geom_bar(data = dplot, aes(x = part, y = lx.diff, fill = group), width = 0.6,
-             col = 'black', position = position_dodge(width = 0.6), stat = 'summary', fun = 'mean', alpha = 0.8)
-  # p
-  # if(type == 'box'){
-  #   p = p +
-  #     geom_boxplot(data=dplot, aes(x = variable, y = value-gmean, fill = group),
-  #                  width = 0.5)
-  # }
-  # if(type == 'bar'){
-  #   p = p +
-  #     geom_bar(aes(x = variable, y = mean, fill = group), width = 0.75,
-  #              col = NA, position = position_dodge(), stat = 'identity')
-  # }
-  # if(type == 'line'){
-  #   W = 0
-  #   p = p +
-  #     geom_point(aes(x = variable, y = mean, col = group, group = group),
-  #                position = position_dodge(width=W)) +
-  #     geom_line(aes(x = variable, y = mean, col = group, group = group),
-  #               linetype = 'dotted')
-  # }
-  p +
-    facet_wrap(~part, nrow = 1, drop = TRUE, scales = 'free_x') +
-    theme_minimal() +
-    theme(panel.grid.minor.x = element_blank(),
-          panel.grid.major.x = element_blank(),
-          axis.text.x = element_blank()) +
-    labs(title = title, x = xlab, y = ylab, fill = group_label)
+  if(x_show_parts){
+    if(facets){
+      p = ggplot() +
+        geom_hline(yintercept = 0) +
+        geom_bar(data = dplot, aes(x = part, y = lx.diff, fill = group), width = 0.6,
+                 col = 'black', position = position_dodge(width = 0.6), stat = 'summary', fun = 'mean', alpha = 0.8) +
+        facet_wrap(~part, nrow = 1, drop = TRUE, scales = 'free_x') +
+        theme_minimal() +
+        theme(panel.grid.minor.x = element_blank(),
+              panel.grid.major.x = element_blank(),
+              axis.text.x = element_blank()) +
+        labs(title = title, x = xlab, y = ylab, fill = group_label)
+    }else{
+      p = ggplot() +
+        geom_hline(yintercept = 0) +
+        geom_bar(data = dplot, aes(x = part, y = lx.diff, fill = group), width = 0.6,
+                 col = 'black', position = position_dodge(width = 0.6), stat = 'summary', fun = 'mean', alpha = 0.8) +
+        theme_minimal() +
+        theme(panel.grid.minor.x = element_blank(),
+              panel.grid.major.x = element_blank(),
+              axis.text.x = element_text(colour = 'black')) +
+        scale_x_discrete(position = 'top') +
+        labs(title = title, x = xlab, y = ylab, fill = group_label)
+    }
+  }else{
+    if(facets){
+      p = ggplot() +
+        geom_hline(yintercept = 0) +
+        geom_bar(data = dplot, aes(x = group, y = lx.diff, fill = part), width = 0.6,
+                 col = 'black', position = position_dodge(width = 0.6), stat = 'summary', fun = 'mean', alpha = 0.8) +
+        facet_wrap(~group, nrow = 1, drop = TRUE, scales = 'free_x') +
+        theme_minimal() +
+        theme(panel.grid.minor.x = element_blank(),
+              panel.grid.major.x = element_blank(),
+              axis.text.x = element_blank()) +
+        labs(title = title, x = xlab, y = ylab, fill = group_label)
+    }else{
+      p = ggplot() +
+        geom_hline(yintercept = 0) +
+        geom_bar(data = dplot, aes(x = group, y = lx.diff, fill = part), width = 0.6,
+                 col = 'black', position = position_dodge(width = 0.6), stat = 'summary', fun = 'mean', alpha = 0.8) +
+        theme_minimal() +
+        theme(panel.grid.minor.x = element_blank(),
+              panel.grid.major.x = element_blank(),
+              axis.text.x = element_text(colour = 'black')) +
+        scale_x_discrete(position = 'top') +
+        labs(title = title, x = xlab, y = ylab, fill = group_label)
+    }
+  }
+  p
 }
